4c7a6f7f9630882962851e1d598d5bad523a54a2
gbs4ever <gbs4ever2@gmail.com>
2019-06-03 19:19:35 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -6,14 +6,16 @@ export function manageFriends(state, action){
         ...state.friends, action.friend
       
       ]}
-     
+     ///review this again we find the index using find index then slice  
+     //to get two protions of the array and we speard those two , to build the new array 
+     // another option might be splice 
     case 'REMOVE_FRIEND':
       const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
       return {
         ...state,
         friends: [
-          ...state.friends.slice(0, removalIndex),
-          ...state.friends.slice(removalIndex+ 1)
+         // ...state.friends.slice(0, removalIndex),
+        ///  ...state.friends.slice(removalIndex+ 1)
         ],
       }
 
